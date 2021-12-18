67259a59626359ff0f15ca852d138c0eb80c618d
izuane <izunnaaneke@googlemail.com>
2021-12-18 21:35:23 +0000

    Add my progress with react refresher.

---
+++ section2-react-refresher/react-course/Meetup_React_App/src/App.js
@@ -0,0 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+import Layout from "./components/layouts/Layout"
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
